04bed5596fb7c44c3f4af426f78c1faf5dbcc4c6
fahmi ganteng <fahmibanjar12@gmail.com>
2025-06-25 09:00:58 +0800

    .

--- app/Http/Controllers/TopologiController.php
+++ app/Http/Controllers/TopologiController.php
@@ -8,7 +8,7 @@ use App\Helpers\topologi;
 class TopologiController extends Controller
 {
     public function startQuiz(){
-        $durasiEvaluasi = 15;
+        $durasiEvaluasi = 5;
         $soalQuiz = topologi::getQuestion();
         $startTime =date("Y-m-d H:i:s");
         $endtime = date("Y-m-d H:i:s",strtotime("+$durasiEvaluasi minutes"));
