f6c5f260d544cbdcb4f8868547f5d25bf5cba33a
fahmi ganteng <fahmibanjar12@gmail.com>
2025-07-04 01:46:03 +0800

    .

--- resources/views/latihan_osi.blade.php
+++ resources/views/latihan_osi.blade.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/konva@8.4.3/konva.min.js"></script>
     <script src="{{ asset('js/latihan_osi.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 </head>
 
 <body>
